7b1d61072062c56c1ae4d0e2ff9d529c914d74d6
LevyDevCode <joaquincoaritialiaga@gmail.com>
2026-01-02 11:12:44 -0400

    Initial commit - REST API

---
+++ app.js
@@ -0,0 +1,162 @@
+const express = require('express') // require -> commonJS
+const crypto = require('node:crypto')
+const cors = require('cors')
+
+const movies = require('./movies.json')
+const z = require('zod')
+const { valid, minVersion } = require('semver')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+const { call } = require('function-bind')
+
+const app = express()
+app.use(express.json())
+app.use(cors({
+    origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+    'http://localhost:8080',
+    'http://localhost:1234',
+    'http://movies.com',
+    'http://midu.dev'
+    ]
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true)
+    }
+
+    if (!origin) {
+        return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+    }
+}))
+app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
+
+//metodos normales: GET/HEAD/POST
+//metodos complejos: PUT/PATCH/DELETE
+
+//CORS PRE-Flight
+//OPTIONS
+
+/**
+ * UNA VEZ USANDO CORS LO PODRIAMOS TRASLADAR ARRIBA 
+ * const ACCEPTED_ORIGINS = [
+    'http://localhost:8080',
+    'http://localhost:1234',
+    'http://movies.com',
+    'http://midu.dev'
+]
+*/
+app.use(express.static('web'))
+
+//todos los recursos que sean MOVIES se identifican con /movies
+app.get('/movies', (req, res) => {
+
+    /**
+     * CUANDO USAMOS EL MIDDLEWARE CORS PODEMOS BORRAR TODA ESTA SECCION DE CODIGO
+     * const origin = req.header('origin')
+    //cuando la peticion es del mismo ORIGIN
+    //http://localhost:1234 -> http://localhost:1234
+    if (ACCEPTED_ORIGINS.includes(origin) || !origin){
+    res.header('Access-Control-Allow-Origin', origin)
+    }*/
+
+    const { genre } = req.query
+    if (genre) {
+        const filteredMovies = movies.filter(
+        movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+        )
+        return res.json(filteredMovies)
+    }
+    res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+    const { id } = req.params
+    const movie = movies.find(movie => movie.id === id)
+    if (movie) return res.json(movie)
+    res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+    const result = validateMovie(req.body)
+
+    if (!result.success) {
+        // 422 Unprocessable Entity
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+ 
+    //en base de datos
+    const newMovie = {
+        id: crypto.randomUUID(), // esto te crea un uuid v4
+        ...result.data // ❌req.body
+    }
+
+    // esto no seria REST, porque estamos guardando
+    // el estado de la aplicacion en memoria
+    movies.push(newMovie)
+
+    res.status(201).json(newMovie) // actualizar la cache del cliente
+})
+
+app.delete('/movies/:id', (req, res) => {
+    /**
+     * CUANDO USAMOS EL MIDDLEWARE CORS PODEMOS BORRAR TODA ESTA SECCION DE CODIGO
+     * const origin = req.header('origin')
+    
+    if (ACCEPTED_ORIGINS.includes(origin) || !origin){
+    res.header('Access-Control-Allow-Origin', origin)
+    }*/
+
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) { 
+        return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    movies.splice(movieIndex, 1)
+
+    return res.json({ success: true, message: 'Movie deleted' })
+    //return res.json({ message: 'Movie delete'})
+})
+
+app.patch('/movies/:id', (req, res) => {
+    const result = validatePartialMovie(req.body)
+
+    if (!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message)})
+    }
+
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found' })
+        } 
+        
+        const updateMovie = {
+            ...movies[movieIndex],
+            ...result.data
+        }
+
+        movies[movieIndex] = updateMovie
+        
+        return res.json(updateMovie)
+     })
+
+     /**CUANDO USAMOS EL MIDDLEWARE CORS PODEMOS BORRAR TODA ESTA SECCION DE CODIGO
+      * app.options('/movies/:id', (req, res) => {
+        const origin = req.header('origin')
+        if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+            res.header('Access-Control-Allow-Origin', origin)
+            res.header('Access-Control-Methods', 'GET, POST, PUT, PATCH, DELETE')
+        }
+        res.send(200)
+     })*/
+
+
+    const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`)
+})
